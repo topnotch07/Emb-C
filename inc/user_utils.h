0acfb66d7d8419c022977392e9c49c52b626c835
THIRUVIKRAMAN B <thiruvikraman07@gmail.com>
2021-04-23 20:12:47 +0530

    Update user_utils.h

--- inc/user_utils.h
+++ inc/user_utils.h
@@ -2,12 +2,10 @@
 #define __USER_UTILS_H__
 /**
  * @file user_utils.h
- * @author Bharath.G ()
+ * @author Thiruvikraman ()
  * @brief User defined utilities used across the project
  * @version 0.1
  * @date 2021-04-23
- * 
- * @copyright Copyright (c) 2021
  * 
  */
 #include "project_config.h"
