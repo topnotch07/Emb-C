4b31631ca039a2d6364d45d1c1e0e80ce12dfbde
THIRUVIKRAMAN B <thiruvikraman07@gmail.com>
2021-04-29 14:38:32 +0530

    Update LedOnOff.c

--- src/LedOnOff.c
+++ src/LedOnOff.c
@@ -6,11 +6,11 @@
 void Led_On()
 {
 
-    PORTB|=(1<<PB0);
+    PORTB|=(1<<PB0); //Led On at mentioned Pin(PB0)
 }
 
 void LedOff()
 {
-   PORTB&=~(1<<PB0);
+   PORTB&=~(1<<PB0); // Led turn OFF 
 }
 
