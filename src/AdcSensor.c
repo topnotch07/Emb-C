c83ed44981b34f56839cdc2a66557f87ffc76302
THIRUVIKRAMAN B <thiruvikraman07@gmail.com>
2021-04-29 12:49:45 +0530

    Update AdcSensor.c

--- src/AdcSensor.c
+++ src/AdcSensor.c
@@ -7,4 +7,14 @@ void AdcSesnor()
         ADMUX|=(1<<REFS0);
         ADCSRA|=(1<<ADEN)|(1<<ADPS0);
     }
+uint16_t ReadAdc(uint8_t ch)
+    {
+        ADMUX&=0xf8;
+        ch&=0x7;
+        ADMUX|=ch;
+        ADCSRA|=(1<<ADSC);
+        while(!(ADCSRA&(1<<ADIF)));
+        ADCSRA|=(1<<ADIF);
+        return(ADC);
+    }
 
