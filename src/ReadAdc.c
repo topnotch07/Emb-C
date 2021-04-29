3c320f7facbd18b07fd4650e216eef36f1d76e8c
THIRUVIKRAMAN B <thiruvikraman07@gmail.com>
2021-04-29 12:49:44 +0530

    Update ReadAdc.c

--- src/ReadAdc.c
+++ src/ReadAdc.c
@@ -3,13 +3,4 @@
 #include"Registors.h"
 #include"port.h"
 
-uint16_t ReadAdc(uint8_t ch)
-    {
-        ADMUX&=0xf8;
-        ch&=0x7;
-        ADMUX|=ch;
-        ADCSRA|=(1<<ADSC);
-        while(!(ADCSRA&(1<<ADIF)));
-        ADCSRA|=(1<<ADIF);
-        return(ADC);
-    }
+
