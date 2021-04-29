ce47822fe05d908bae7df028ce9048fa08f3f83a
THIRUVIKRAMAN B <thiruvikraman07@gmail.com>
2021-04-29 12:28:59 +0530

    Update LedOnOff.c

--- src/LedOnOff.c
+++ src/LedOnOff.c
@@ -2,19 +2,6 @@
 #include"Uart.h"
 #include"Registors.h"
 #include"port.h"
-#include<avr/interrupt.h>
-
-
-unsigned volatile flag=0,flag1=0;
-
-ISR(INT0_vect)
-{
-    flag=1;
-}
-ISR(INT1_vect)
-{
-    flag1=1;
-}
 
 void Led_On()
 {
