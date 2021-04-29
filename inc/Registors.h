f28fba24e85246c2859ca501d781528395faa0ca
THIRUVIKRAMAN B <thiruvikraman07@gmail.com>
2021-04-29 11:40:53 +0530

    Update Registors.h

--- inc/Registors.h
+++ inc/Registors.h
@@ -1,3 +1,13 @@
+/**
+ * @file Registors.h
+ * @author Thiruvikraman B
+ * @brief Include header file for activity 3
+ * @version 0.1
+ * @date 2021-04-29
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
 #ifndef REGISTORS_H_INCLUDED
 #define REGISTORS_H_INCLUDED
 
