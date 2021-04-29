0819dc8d05f4f219b539246da7350f9ace3d1d05
THIRUVIKRAMAN B <thiruvikraman07@gmail.com>
2021-04-29 14:36:55 +0530

    Update AdcSensor.c

--- src/AdcSensor.c
+++ src/AdcSensor.c
@@ -4,16 +4,18 @@
 #include"port.h"
 void AdcSesnor()
     {
-        ADMUX|=(1<<REFS0);
-        ADCSRA|=(1<<ADEN)|(1<<ADPS0);
+        ADMUX|=(1<<REFS0);    //ADMUX register and 0 Aref connected to AVCC internally in MCU
+        ADCSRA|=(1<<ADEN)|(1<<ADPS0);    // Enabling ADC and setting the sampling frequency through ADCSRA registor
     }
 uint16_t ReadAdc(uint8_t ch)
     {
-        ADMUX&=0xf8;
-        ch&=0x7;
-        ADMUX|=ch;
-        ADCSRA|=(1<<ADSC);
-        while(!(ADCSRA&(1<<ADIF)));
+        ADMUX&=0xf8;  // Selecting channel 0 by setting 1 to all ADMUX register except Bit 0,1,2
+        ch&=0x7;      // logical AND with 0x7 (binary representation: 0b00000111) will give the channel itsef
+        ADMUX|=ch;   // selecting the channel by OR with ADMUX.
+        ADCSRA|=(1<<ADSC);      // Start single conversion by enabling Bit 6 in ADCSRA registor
+
+        while(!(ADCSRA&(1<<ADIF)));      // conversion
+
         ADCSRA|=(1<<ADIF);
         return(ADC);
     }
