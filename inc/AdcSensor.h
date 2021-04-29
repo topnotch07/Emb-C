9417769c27b0845200ad117302a49cee70a2ff37
THIRUVIKRAMAN B <thiruvikraman07@gmail.com>
2021-04-29 11:39:25 +0530

    Update AdcSensor.h

--- inc/AdcSensor.h
+++ inc/AdcSensor.h
@@ -1,3 +1,13 @@
+/**
+ * @file AdcSensor.h
+ * @author Thiruvikraman B
+ * @brief Include header file for activity 2
+ * @version 0.1
+ * @date 2021-04-29
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
 #ifndef ADCSENSOR_H_INCLUDED
 #define ADCSENSOR_H_INCLUDED
 
