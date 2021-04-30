7b6e3c50f832a432b8de617dc248cf7953005746
THIRUVIKRAMAN B <thiruvikraman07@gmail.com>
2021-04-30 14:09:33 +0530

    Update port.c

--- src/port.c
+++ src/port.c
@@ -7,15 +7,13 @@
  void port()
  {
     DDRB |= (1<<PB0); //setting D port as output for led
-    DDRD &=~(1<<PD2); // setting input obtain from button in occupancy of seat 
-    DDRD &=~(1<<PD3); // setting input of a button Heater switch
+    DDRD &=~(1<<PD0); // setting input obtain from button in occupancy of seat 
+    DDRD &=~(1<<PD1); // setting input of a button Heater switch
 
-    PORTD|=(1<<PD2); //configure pin PD2 as an input- DDRD-'0' and PORTD-'1'
-    PORTD|=(1<<PD3); //configure pin PD3 as an input- DDRD-'0' and PORTD-'1'
+    PORTD|=(1<<PD0); //configure pin PD2 as an input- DDRD-'0' and PORTD-'1'
+    PORTD|=(1<<PD1); //configure pin PD3 as an input- DDRD-'0' and PORTD-'1'
 
-    EICRA|=(1<<ISC00)|(1<<ISC10); // interupt 
-    EIMSK|=(1<<INT0)|(1<<INT1);   // mask for interrupt 
-    sei(); 
+    
  }
 
 
