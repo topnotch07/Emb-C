c2b22693cb51ef9f14434122cc43dfc848308a9c
THIRUVIKRAMAN B <thiruvikraman07@gmail.com>
2021-04-30 14:55:32 +0530

    Update port.h

--- inc/port.h
+++ inc/port.h
@@ -12,9 +12,31 @@
 
 #ifndef PORT_H_INCLUDED
 #define PORT_H_INCLUDED
-void LedOff(void);
-void Led_On(void);
-void port(void);
+#include <avr/io.h>
+#include "port.h"
+
+//Macro definition 
+
+#define LED_ON 	(0x01)			/**< LED state HIGH */
+#define LED_OFF	(0x00)			/**< LED state LOW */
+
+
+#define LED_PORT (PORTB)    /**< LED Port Number */
+#define LED_PIN  (PORTB0)   /**< LED Pin number  */
+#define SENSOR_ON !(PIND&(1<<PD0)) // Switch1 ON
+#define HEAT_ON !(PIND&(1<<PD1)) // Switch2 ON
+
+/**
+ * Function Definitions
+ */
+
+/**
+ * @brief Change the state of the LED Pin according to the value of state
+ * 
+ * @param state Pin level to which the LED Pin should be set
+ */
+void ledstat(uint8_t state);
+void InitLED(void);
 
 #endif // PORT_H_INCLUDED
 
